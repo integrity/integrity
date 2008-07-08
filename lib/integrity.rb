101f2d7744831739d3bc15f5fcce5d48e6bef6ae
Nicolás Sanguinetti <contacto@nicolassanguinetti.info>
2008-07-08 01:53:40 -0300

    Sample web UI using sinatra

--- lib/integrity.rb
+++ lib/integrity.rb
@@ -1,2 +1,7 @@
+__DIR__ = File.dirname(__FILE__)
+$:.unshift "#{__DIR__}/sinatra/lib", "#{__DIR__}/integrity"
+
+require "rubygems"
+
 module Integrity
 end
---
+++ lib/integrity/web_interface.rb
@@ -0,0 +1,3 @@
+get "/" do
+  "Welcome to Integrity"
+end
