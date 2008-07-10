cac5fa00e1359a7d06b3105cc43d890e9abecc17
Nicolás Sanguinetti <contacto@nicolassanguinetti.info>
2008-07-10 03:58:11 -0300

    Fix autoload for Project, instead of Models

--- lib/integrity.rb
+++ lib/integrity.rb
@@ -17,7 +17,7 @@ module Integrity
     DataMapper.setup(:default, config[:database_uri])
   end
 
-  autoload :Models,  'models'
+  autoload :Project, 'project'
   autoload :Builder, 'builder'
   autoload :SCM,     'scm'
 
