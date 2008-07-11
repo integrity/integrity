a543bcae1360e7545bc4b18fb684d0080475ba58
Simon Rozet <simon@rozet.name>
2008-07-11 23:02:49 +0200

    fix git-dir (append /.git) and stderr/out.read

--- lib/integrity/scm/git.rb
+++ lib/integrity/scm/git.rb
@@ -11,8 +11,8 @@ module Integrity
 
       def checkout(destination)
         execute "clone --depth 1 #{@uri.to_s} #{destination}"
-        execute "--git-dir=#{destination} checkout #{@branch}"
-        execute "--git-dir=#{destination} pull"
+        execute "--git-dir=#{destination}/.git checkout #{@branch}"
+        execute "--git-dir=#{destination}/.git pull"
         true
       rescue RuntimeError
         false
@@ -20,8 +20,8 @@ module Integrity
 
         def execute(command)
           Open3.popen3 "git #{command}" do |_, stdout, stderr|
-            @logger.output << stdout
-            @logger.error << stderr
+            @logger.output << stdout.read
+            @logger.error << stderr.read
             raise unless $?.success?
           end
         end
--- spec/scm/git_spec.rb
+++ spec/scm/git_spec.rb
@@ -4,13 +4,19 @@ require File.dirname(__FILE__) + '/../../lib/integrity/scm/git'
 describe Integrity::SCM::Git do
   before(:each) do
     @uri = Addressable::URI.parse('git://github.com/foca/integrity.git')
-    @build = mock('build model', :error => '', :output => '', :status => true)
+    @build = mock('build model',
+      :output => '',
+      :error  => '',
+      :status => true
+    )
     @scm = Integrity::SCM::Git.new(@uri, 'master', @build)
   end
 
   describe 'When checking-out a repository' do
     before(:each) do
       Open3.stub!(:popen3)
+      @stdout = mock('io', :read => 'out')
+      @stderr = mock('io', :read => 'err')
       $?.stub!(:success?).and_return(true)
     end
 
@@ -22,12 +28,12 @@ describe Integrity::SCM::Git do
 
     it 'should switch to the specified branch' do
       Open3.should_receive(:popen3).
-        with('git --git-dir=/foo/bar checkout master')
+        with('git --git-dir=/foo/bar/.git checkout master')
       @scm.checkout('/foo/bar')
     end
 
     it 'should fetch updates' do
-      Open3.should_receive(:popen3).with('git --git-dir=/foo/bar pull')
+      Open3.should_receive(:popen3).with('git --git-dir=/foo/bar/.git pull')
       @scm.checkout('/foo/bar')
     end
 
@@ -37,13 +43,13 @@ describe Integrity::SCM::Git do
     end
 
     it "should write stdout to build's output" do
-      Open3.stub!(:popen3).and_yield('', 'foo', '')
-      @build.output.should_receive(:<<).with('foo').exactly(3).times
+      Open3.stub!(:popen3).and_yield('', @stdout, @stderr)
+      @build.output.should_receive(:<<).with('out').exactly(3).times
       @scm.checkout('/foo/bar')
     end
 
     it "should write stderr to build's error" do
-      Open3.stub!(:popen3).and_yield('', '', 'err')
+      Open3.stub!(:popen3).and_yield('', @stdout, @stderr)
       @build.error.should_receive(:<<).with('err').exactly(3).times
       @scm.checkout('/foo/bar')
     end
