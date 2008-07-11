80a8de732e072f7458cef31d81942eadb4ad0028
Simon Rozet <simon@rozet.name>
2008-07-11 19:45:22 +0200

    drop scm::result for real this time

--- lib/integrity/scm.rb
+++ lib/integrity/scm.rb
@@ -6,15 +6,5 @@ module Integrity
     rescue LoadError, NameError
       raise "could not find any SCM named `#{scm}'"
     end
-
-    Result = Struct.new(:output, :error, :status) do
-      %w(success failure).each do |state|
-        define_method("#{state}?") { self.status == state }
-        define_method("#{state}!") do
-          self.status = state
-          self
-        end
-      end
-    end
   end
 end
