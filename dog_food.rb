19d1d409f87701bbbbb19dda40f39930cbd58420
Simon Rozet <simon@rozet.name>
2008-07-12 02:39:55 +0200

    dog fooding!

---
+++ dog_food.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/lib/integrity'
+
+Integrity.new
+project = Integrity::Project.new
+project.name = 'Integrity'
+project.uri = Addressable::URI.parse('git://github.com/foca/integrity.git')
+project.command = 'git-submodule init && git-submodule update && rake spec'
+
+result = project.build
+
+puts "Error\n#{'='*6}"
+puts result.error
+puts "Output\n#{'='*6}"
+puts result.output
+puts "STATUS: #{result.human_readable_status}"
