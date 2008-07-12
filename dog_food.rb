167a9e3816984e1513d590f52c19ec3e332cc402
Nicolás Sanguinetti <contacto@nicolassanguinetti.info>
2008-07-12 04:12:17 -0300

    Make sure dog_food saves to the database so it automatically creates the URI object, else it fails miserably

--- dog_food.rb
+++ dog_food.rb
@@ -1,11 +1,14 @@
 require File.dirname(__FILE__) + '/lib/integrity'
 
 Integrity.new
-project = Integrity::Project.new
-project.name = 'Integrity'
-project.uri = 'git://github.com/foca/integrity.git'
-project.command = 'git-submodule init && git-submodule update && rake spec'
 
+Integrity::Project.create do |p|
+  p.name = 'Integrity'
+  p.uri = 'git://github.com/foca/integrity.git'
+  p.command = 'git-submodule init && git-submodule update && rake spec'
+end
+
+project = Integrity::Project.first(:name => 'Integrity')
 result = project.build
 
 puts "Error\n#{'='*6}"
