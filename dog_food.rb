966bc8f48acb44bda6a9a50d40c54a7920b9d1f3
Nicolás Sanguinetti <contacto@nicolassanguinetti.info>
2008-07-20 05:09:15 -0300

    w00t, dog food runs

--- dog_food.rb
+++ dog_food.rb
@@ -1,18 +1,19 @@
 require File.dirname(__FILE__) + '/lib/integrity'
 
 Integrity.new
+include Integrity
 
-Integrity::Project.create(
-  :name    => 'Integrity', 
-  :uri     => 'git://github.com/foca/integrity.git',
-  :command => 'echo `pwd`; git-submodule update --init && rake'
-)
-
-project = Integrity::Project.first(:name => 'Integrity')
+project = Project.first(:name => "Integrity") || begin
+  Integrity::Project.create(
+    :name    => 'Integrity', 
+    :uri     => 'git://github.com/foca/integrity.git',
+    :command => 'git-submodule update --init && rake'
+  )
+  Project.first(:name => 'Integrity')
+end
+  
 result = project.build
 
-puts "Error\n#{'='*6}"
-puts result.error
 puts "Output\n#{'='*6}"
 puts result.output
 puts "STATUS: #{result.human_readable_status}"
