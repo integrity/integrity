40d1e6a297532b948121ee6400b3522937483175
Nicolás Sanguinetti <contacto@nicolassanguinetti.info>
2008-07-20 22:14:51 -0300

    Fix bash color escaping [#2 state:resolved]

--- lib/integrity/ui/web.rb
+++ lib/integrity/ui/web.rb
@@ -116,13 +116,13 @@ helpers do
   end
   
   def bash_color_codes(string)
-    string.gsub("[0m", '</span>').
-      gsub("[31m", '<span class="color31">').
-      gsub("[32m", '<span class="color32">').
-      gsub("[33m", '<span class="color33">').
-      gsub("[34m", '<span class="color34">').
-      gsub("[35m", '<span class="color35">').
-      gsub("[36m", '<span class="color36">').
-      gsub("[37m", '<span class="color37">')
+    string.gsub("\e[0m", '</span>').
+      gsub("\e[31m", '<span class="color31">').
+      gsub("\e[32m", '<span class="color32">').
+      gsub("\e[33m", '<span class="color33">').
+      gsub("\e[34m", '<span class="color34">').
+      gsub("\e[35m", '<span class="color35">').
+      gsub("\e[36m", '<span class="color36">').
+      gsub("\e[37m", '<span class="color37">')
   end
 end
