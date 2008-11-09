2a03e2a1d0e058454700df603f825f58128950b9
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2008-11-09 06:45:45 -0200

    Upgrade rspec to 1.1.8

---
+++ vendor/rspec-1.1.8/lib/spec/example/example_group.rb
@@ -0,0 +1,24 @@
+module Spec
+  module Example
+    # Base class for customized example groups. Use this if you
+    # want to make a custom example group.
+    class ExampleGroup
+      extend Spec::Example::ExampleGroupMethods
+      include Spec::Example::ExampleMethods
+
+      def initialize(defined_description, &implementation)
+        @_defined_description = defined_description
+        @_implementation = implementation || pending_implementation
+      end
+      
+    private
+      
+      def pending_implementation
+        error = NotYetImplementedError.new(caller)
+        lambda { raise(error) }
+      end
+    end
+  end
+end
+
+Spec::ExampleGroup = Spec::Example::ExampleGroup
