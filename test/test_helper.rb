33b93cb8bbecd8b7a247bfdca931dd04de807710
Simon Rozet <simon@rozet.name>
2008-12-13 21:07:46 +0100

    better #describe, alow to use #specify in context and test helper to setup database

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
-require File.dirname(__FILE__) + '/../lib/integrity'
-require File.dirname(__FILE__) + '/test_fixtures'
+require File.dirname(__FILE__) + "/../lib/integrity"
+require File.dirname(__FILE__) + "/test_fixtures"
 
 require "ruby-debug"
 require "test/unit"
@@ -9,12 +9,24 @@ require "matchy"
 require "rr"
 
 class Test::Unit::TestCase
-  include RR::Adapters::TestUnit
+  class << self
+    alias_method :specify, :test
+  end
 end
 
 # Gives a nicer syntax than declaring TestCase subclasses in tests
 def describe(name, &block)
-  Class.new(Test::Unit::TestCase) do
-    context(name.to_s, &block)
+  Test::Unit::TestCase.context(name, &block)
+end
+
+module TestHelper
+  def setup_and_reset_database!
+    DataMapper.setup(:default, "sqlite3::memory:")
+    DataMapper.auto_migrate!
   end
+end
+
+class Test::Unit::TestCase
+  include RR::Adapters::TestUnit
+  include TestHelper
 end
