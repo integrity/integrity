314e678175b3450e00df7dace6aa9465311d4b02
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2008-12-15 01:31:57 -0200

    Add 'have' expectation (@collection.should have(n).elements)

--- test/helpers/expectations.rb
+++ test/helpers/expectations.rb
@@ -1,3 +1,4 @@
 require File.dirname(__FILE__) + "/expectations/be_a"
 require File.dirname(__FILE__) + "/expectations/change"
+require File.dirname(__FILE__) + "/expectations/have"
 require File.dirname(__FILE__) + "/expectations/predicates"
