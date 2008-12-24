98f8080c6e830f57054608909cf7d8ccc18bbfe9
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2008-12-24 03:46:45 -0200

    Remove duplicated code

--- test/helpers/acceptance/webrat.rb
+++ test/helpers/acceptance/webrat.rb
@@ -25,27 +25,3 @@ end
 class Webrat::SinatraSession
   attr_reader :response
 end
-
-module PrettyStoryPrintingHelper
-  def self.included(base)
-    base.before(:all) do
-      puts
-      print "\e[36m"
-      puts  self.class.story.to_s.gsub(/^\s+/, '')
-      print "\e[0m"
-    end
-
-    base.after(:all) do
-      puts
-    end    
-    
-    base.extend ClassMethods
-  end
-  
-  module ClassMethods
-    def story(story=nil)
-      @story = story if story
-      @story
-    end  
-  end
-end
