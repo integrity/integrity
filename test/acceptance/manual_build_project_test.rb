62ccb66be9ab40c4e9fe1240d6333028fc301109
Simon Rozet <simon@rozet.name>
2008-12-29 10:49:03 +0100

    enable failing test and fix them
    This reverts commit ea6ef1b6922fa21c02e1e251cd83f09409ac15a7.
    
    Conflicts:

--- test/acceptance/manual_build_project_test.rb
+++ test/acceptance/manual_build_project_test.rb
@@ -12,6 +12,7 @@ class ManualBuildProjectTest < Test::Unit::AcceptanceTestCase
     setup_log!
     create_git_repository!
     set_and_create_export_directory!
+    login_as "admin", "test"
   end
 
   after(:all) do
@@ -20,37 +21,33 @@ class ManualBuildProjectTest < Test::Unit::AcceptanceTestCase
   end
 
   scenario "a user clicking on 'Manual Build' trigger a build that is successful" do
-    return
-
     project = Project.gen(:my_test_project)
 
-    get "/#{project.permalink}"
-    body.should =~ /No builds for this project, buddy/
+    visit "/#{project.permalink}"
+    response_body.should include("No builds for this project, buddy")
 
     lambda do
       post "/#{project.permalink}/builds"
-      status.should == 200
+      response_code.should == 200
     end.should change(project.builds, :count).from(0).to(1)
 
-    get "/#{project.permalink}"
-    body.should have_tag("h1", /Built\s*#{project.last_build.short_commit_identifier}\s*successfully/m)
-    body.should have_tag("blockquote p", "readme")
-    body.should have_tag("span.who")
-    body.should have_tag("span.when", /today/)
-    body.should have_tag("pre.output", /this is just because Build/)
+    visit "/#{project.permalink}"
+    response_body.should have_tag("h1", /Built\s*#{project.last_build.short_commit_identifier}\s*successfully/m)
+    response_body.should have_tag("blockquote p", "readme")
+    response_body.should have_tag("span.who")
+    response_body.should have_tag("span.when", /today/)
+    response_body.should have_tag("pre.output", /this is just because Build/)
   end
 
   scenario "a user clicks on 'Manual Build' and trigger a build that is unsuccessful" do
-    return
-
     project = Project.gen(:my_test_project, :command => "ruby not-found.rb")
 
     lambda do
       post "/#{project.permalink}/builds"
-      status.should == 200
+      response_code.should == 200
     end.should change(project.builds, :count).from(0).to(1)
 
     get "/#{project.permalink}"
-    body.should have_tag("h1", /Built\s*#{project.last_build.short_commit_identifier}\s*and failed/m)
+    response_body.should have_tag("h1", /Built\s*#{project.last_build.short_commit_identifier}\s*and failed/m)
   end
 end
