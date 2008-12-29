a486b99a6b9c6ae5390f29b36d9c60ec6800a679
Simon Rozet <simon@rozet.name>
2008-12-29 17:43:03 +0100

    basic test for when nuking a project

---
+++ test/acceptance/project_administrativia_test.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + "/../helpers"
+
+class ProjectAdministrativiaTest < Test::Unit::AcceptanceTestCase
+  story <<-EOS
+    As an administrator,
+    I want to be able to edit and delete a repo as well
+    So that well, err, I do admin stuff!
+  EOS
+
+  before(:each) do
+    setup_and_reset_database!
+    enable_auth!
+    login_as "admin", "test"
+  end
+
+  scenario "browsing to a project page and click on 'Yes, I'm sure, nuke it' destroy the project" do
+    project = Project.generate(:integrity, :builds => 4.of { Build.gen })
+    visit "/integrity/edit"
+
+    lambda do
+      click_button "Yes, I'm sure, nuke it"
+    end.should change(Project, :count).from(1).to(0)
+
+    visit "/"
+    response_body.should_not have_tag("ul#projects li a", "Integrity")
+
+    visit "/integrity"
+    response_code.should == 404
+  end
+end
