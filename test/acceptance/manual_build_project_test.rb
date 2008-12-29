e59fe82a2b4fb742d9361d95f7e6d1458c8e3cfe
Simon Rozet <simon@rozet.name>
2008-12-29 13:35:56 +0100

    test for when fixing build command. also, back to #have_tag

--- test/acceptance/manual_build_project_test.rb
+++ test/acceptance/manual_build_project_test.rb
@@ -21,7 +21,6 @@ class ManualBuildProjectTest < Test::Unit::AcceptanceTestCase
   scenario "a user clicking on 'Manual Build' and triggers a successful build" do
     git_repo(:my_test_project).add_successful_commit
     Project.gen(:my_test_project, :uri => git_repo(:my_test_project).path)
-    
     login_as "admin", "test"
 
     visit "/my-test-project"
@@ -29,17 +28,16 @@ class ManualBuildProjectTest < Test::Unit::AcceptanceTestCase
 
     click_button "manual build"
 
-    response_body.should =~ /Built\s+#{git_repo(:my_test_project).head}\s+successfully/
-    response_body.should =~ /This commit will work/          # commit message
-    response_body.should =~ /by:\s+John Doe/                 # commit author
-    response_body.should =~ /today/                          # commit date
-    response_body.should have_tag("pre", /Running tests.../) # build output
+    response_body.should have_tag("h1", /Built\s+#{git_repo(:my_test_project).head}\s+successfully/)
+    response_body.should have_tag("blockquote p", /This commit will work/)  # commit message
+    response_body.should have_tag("span.who",     /by:\s+John Doe/)         # commit author
+    response_body.should have_tag("span.when",    /today/)                  # commit date
+    response_body.should have_tag("pre.output",   /Running tests.../)       # build output
   end
 
   scenario "a user clicking on 'Manual Build' and triggers a failed build" do
     git_repo(:my_test_project).add_failing_commit
     Project.gen(:my_test_project, :uri => git_repo(:my_test_project).path)
-    
     login_as "admin", "test"
     
     visit "/my-test-project"
@@ -47,7 +45,34 @@ class ManualBuildProjectTest < Test::Unit::AcceptanceTestCase
 
     click_button "manual build"
     
-    response_body.should =~ /Built\s+#{git_repo(:my_test_project).head}\s+and failed/
-    response_body.should =~ /This commit will fail/          # commit message
+    response_body.should have_tag("h1", /Built\s+#{git_repo(:my_test_project).head}\s+and failed/)
+    response_body.should have_tag("blockquote p", /This commit will fail/)
+  end
+
+  scenario "fixing the build command and then rebuilding result in a successful build" do
+    git_repo(:my_test_project).add_successful_commit
+    project = Project.gen(:my_test_project, :uri => git_repo(:my_test_project).path,
+                          :command => "ruby not-found.rb")
+    login_as "admin", "test"
+
+    visit "/my-test-project"
+    click_button "manual build"
+    response_body.should have_tag("h1", /failed/)
+
+    # FIXME: command is set to './"escape code for /"test because of either
+    # a bug in Webrat or in app.rb (escaping related)
+=begin
+    visit "/my-test-project/edit"
+    fill_in "Build script", :with => "./test"
+    click_button "Update Project"
+=end
+    project.update_attributes(:command => "./test")
+
+    Project.first(:permalink => "my-test-project").command.should == "./test"
+
+    visit "/my-test-project"
+    click_button "Request Manual Build"
+
+    response_body.should have_tag("h1", /success/)
   end
 end
