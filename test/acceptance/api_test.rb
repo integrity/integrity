8ee3f809f80a8280eb75b7cc806d81daf24bd69c
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-01-02 12:57:33 -0200

    More API tests

--- test/acceptance/api_test.rb
+++ test/acceptance/api_test.rb
@@ -7,17 +7,7 @@ class ApiTest < Test::Unit::AcceptanceTestCase
     So that my project is build is made everytime I push to The Holly Hub
   EOF
 
-  before(:each) do
-    setup_and_reset_database!
-    setup_log!
-    set_and_create_export_directory!
-  end
-
-  after(:all) do
-    destroy_all_git_repos
-  end
-
-  scenario "an unauthenticated request returns a 403" do
+  scenario "it only build commits for the branch being monitored" do
     pending "TODO"
   end
 
@@ -25,15 +15,7 @@ class ApiTest < Test::Unit::AcceptanceTestCase
     pending "TODO"
   end
 
-  scenario "it only build commits for the branch being monitored" do
-    pending "TODO"
-  end
-
-  scenario "receiving a build request with an invalid payload returns an Invalid Request error" do
-    pending "TODO"
-  end
-
-  scenario "receiving a build request with build_all_commits *disabled* only build HEAD" do
+  scenario "receiving a build request with build_all_commits *disabled* only builds HEAD" do
     Integrity.config[:build_all_commits] = false
 
     Project.gen(:my_test_project, :uri => git_repo(:my_test_project).path)
@@ -42,18 +24,35 @@ class ApiTest < Test::Unit::AcceptanceTestCase
     git_repo(:my_test_project).add_successful_commit
     head = git_repo(:my_test_project).head
 
-    login_as "admin", "test"
-
     lambda do
+      basic_auth "admin", "test"
       post "/my-test-project/push", :payload => payload(head, "master")
-    end.should change(Build, :count).from(0).to(1)
+    end.should change(Build, :count).by(1)
 
     response_body.should == "Thanks, build started."
     response_code.should == 200
 
     visit "/my-test-project"
+
     response_body.should =~ /#{git_repo(:my_test_project).short_head} successfully/
     response_body.should =~ /This commit will work/
+  end
+
+  scenario "an unauthenticated request returns a 401" do
+    Project.gen(:my_test_project, :uri => git_repo(:my_test_project).path)
+    head = git_repo(:my_test_project).head
+    post "/my-test-project/push", :payload => payload(head, "master")
+    
+    response_code.should == 401
+  end
+
+  scenario "receiving a build request with an invalid payload returns an Invalid Request error" do
+    Project.gen(:my_test_project, :uri => git_repo(:my_test_project).path)
+
+    basic_auth "admin", "test"
+
+    post "/my-test-project/push", :payload => "foo"
+    response_code.should == 422
   end
 
   def payload(after, branch="master")
