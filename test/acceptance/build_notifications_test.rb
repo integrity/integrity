73d4fe03340f246415e79cce3e401488951061eb
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-01-02 09:45:20 -0200

    Add an acceptance test for checking build notifications

---
+++ test/acceptance/build_notifications_test.rb
@@ -0,0 +1,35 @@
+require File.dirname(__FILE__) + "/../helpers"
+require File.dirname(__FILE__) + "/../helpers/acceptance/textfile_notifier"
+
+class BuildNotificationsTest < Test::Unit::AcceptanceTestCase
+  story <<-EOS
+    As an administrator,
+    I want to setup notifiers on my projects
+    So that I get alerts with every build
+  EOS
+
+  scenario "an admin sets up a notifier for a project that didn't have any" do
+    git_repo(:my_test_project).add_successful_commit
+    Project.gen(:my_test_project, :notifiers => [], :uri => git_repo(:my_test_project).path)
+    rm_f "/tmp/textfile_notifications.txt"
+    
+    login_as "admin", "test"
+    
+    visit "/my-test-project"
+
+    click_link "Edit Project"
+    check "enabled_notifiers_textfile"
+    fill_in "File", :with => "/tmp/textfile_notifications.txt"
+    click_button "Update Project"
+    
+    click_button "manual build"
+    
+    notification = File.read("/tmp/textfile_notifications.txt")
+    notification.should =~ /=== Build #{git_repo(:my_test_project).short_head} was successful ===/
+    notification.should =~ /Build #{git_repo(:my_test_project).head} was successful/
+    notification.should =~ /Commit Author: John Doe/
+    notification.should =~ /Commit Date: (.+)/
+    notification.should =~ /Commit Message: This commit will work/
+    notification.should =~ /Build Output:\n\nRunning tests...\n/
+  end
+end
---
+++ test/helpers/acceptance/textfile_notifier.rb
@@ -0,0 +1,26 @@
+module Integrity
+  class Notifier
+    class Textfile < Notifier::Base
+      def self.to_haml
+        <<-haml
+%p.normal
+  %label{ :for => "textfile_notifier_file" } File
+  %input.text#textfile_notifier_file{ :name => "notifiers[Textfile][file]", :type => "text", :value => config["file"] }
+        haml
+      end
+      
+      def initialize(build, config={})
+        super
+        @file = @config["file"]
+      end
+      
+      def deliver!
+        File.open(@file, "a") do |f|
+          f.puts "=== #{short_message} ==="
+          f.puts
+          f.puts full_message
+        end
+      end
+    end
+  end
+end
