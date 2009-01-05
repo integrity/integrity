91f3d101cedcdee69ea6d9fe47203ec38a6a7506
Simon Rozet <simon@rozet.name>
2009-01-05 18:56:48 +0100

    initial support of migration
    * the UI (err, the thorfile) should probably be improved to reflect this
    * the migration neeeds to be double-checked
    * also, maybe we should add some index?

---
+++ lib/integrity/migrations.rb
@@ -0,0 +1,50 @@
+require "dm-migrations"
+require "migration_runner"
+
+include DataMapper::Types
+
+migration 1, :initial do
+  up do
+    create_table :projects do
+      column :id,         Serial
+      column :name,       String,   :nullable => false
+      column :permalink,  String
+      column :uri,        URI,      :nullable => false
+      column :branch,     String,   :nullable => false, :default => "master"
+      column :command,    String,   :nullable => false, :default => "rake"
+      column :public,     Boolean,                      :default  => true
+      column :building,   Boolean,                      :default  => false
+      column :created_at, DateTime
+      column :updated_at, DateTime
+
+      column :build_id,   Serial
+      column :notifier_id,Serial
+    end
+
+    create_table :builds do
+      column :id,                Serial
+      column :output,            Text,     :nullable => false, :default => ""
+      column :successful,        Boolean,  :nullable => false, :default => false
+      column :commit_identifier, String,   :nullable => false
+      column :commit_metadata,   Yaml,     :nullable => false
+      column :created_at,        DateTime
+      column :updated_at,        DateTime
+
+      column :project_id,        Serial
+    end
+
+    create_table :notifiers do
+      column :id,         Serial
+      column :name,       String, :nullable => false
+      column :config,     Yaml,   :nullable => false
+
+      column :project_id, Serial
+    end
+  end
+
+  down do
+    drop_table :notifiers
+    drop_table :projects
+    drop_table :builds
+  end
+end
