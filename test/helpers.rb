d5d3b724a6f11f2f7cfa91dd35eca692c94959b4
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-01-25 22:10:12 -0200

    Actually, just set /dev/null as the log for integrity during testing
    Better than stubbing it out, since it will fail loudly when something goes wrong

--- test/helpers.rb
+++ test/helpers.rb
@@ -32,8 +32,7 @@ module TestHelper
   end
 
   def ignore_logs!
-    DataMapper.logger = Logger.new("/dev/null")
-    stub(Integrity).log { nil }
+    Integrity.config[:log] = "/dev/null"
   end
 end
 
