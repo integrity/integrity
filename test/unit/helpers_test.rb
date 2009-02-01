483a969bb9db27dc724e736eae59ec95d87ab89e
Justin Knowlden <jaknowlden@gmail.com>
2009-02-01 16:25:17 +0800

    pretty_date wasn't so pretty if today was the first day of the month. 1 - 1 == 0 and Time.mktime no likee
    Signed-off-by: Nicolás Sanguinetti <contacto@nicolassanguinetti.info>

--- test/unit/helpers_test.rb
+++ test/unit/helpers_test.rb
@@ -5,7 +5,7 @@ class BrowsePublicProjectsTest < Test::Unit::TestCase
 
   test "#pretty_date" do
     pretty_date(Time.now).should == "today"
-    pretty_date(Time.mktime(Time.now.year, Time.now.month, Time.now.day-1)).should == "yesterday"
+    pretty_date(Time.new - 86400).should == "yesterday"
 
     pretty_date(Time.mktime(1995, 12, 01)).should == "on Dec 01st"
     pretty_date(Time.mktime(1995, 12, 21)).should == "on Dec 21st"
