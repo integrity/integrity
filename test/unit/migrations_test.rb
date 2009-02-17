a9fad956dee7ff487dc66ebdd66ea70bd789ff1f
Simon Rozet <simon@rozet.name>
2009-02-17 19:18:05 +0100

    Also test for when migrating down

--- test/unit/migrations_test.rb
+++ test/unit/migrations_test.rb
@@ -1,12 +1,16 @@
 require File.dirname(__FILE__) + "/../helpers"
 
 class MigrationsTest < Test::Unit::TestCase
+  def database_adapter
+    DataMapper.repository(:default).adapter
+  end
+
   def table_exists?(table_name)
     database_adapter.storage_exists?(table_name)
   end
 
-  def database_adapter
-    DataMapper.repository(:default).adapter
+  def current_migrations
+    database_adapter.query("SELECT * from migration_info")
   end
 
   before(:all) do
@@ -15,34 +19,39 @@ class MigrationsTest < Test::Unit::TestCase
 
   before(:each) do
     [Project, Build, Commit, Notifier].each(&:auto_migrate_down!)
+    assert !table_exists?("migration_info") # just to be sure
   end
 
-  test "migrating up for the first time creates the database up to last migration" do
-    assert !table_exists?("migration_info")
+  test "migrating up a pre migration database" do
     Integrity.migrate("up")
 
-    database_adapter.query("SELECT * from migration_info").
-      should == ["initial", "add_commits"]
+    current_migrations.should == ["initial", "add_commits"]
     assert table_exists?("integrity_projects")
     assert table_exists?("integrity_builds")
     assert table_exists?("integrity_notifiers")
     assert table_exists?("integrity_commits")
   end
 
-  context "Migrating a pre migration database" do
-    it "creates the migration_info table" do
-      [Project, Build, Notifier].each(&:auto_migrate!)
-      assert !table_exists?("migration_info")
-      assert !table_exists?("integrity_commits")
+  test "migrating down a pre migration database" do
+    [Project, Build, Notifier].each(&:auto_migrate!)
 
-      Integrity.migrate("up")
+    Integrity.migrate("down")
 
-      assert table_exists?("migration_info")
-      database_adapter.query("SELECT * from migration_info").should == ["initial"]
-    end
+    # Initial migration is created but it gets removed just after
+    # because we migrate down
+    assert table_exists?("migration_info")
+    current_migrations.should == []
 
-    it "migrates the data as well" do
-      pending "42"
-    end
+    assert ! table_exists?("integrity_projects")
+    assert ! table_exists?("integrity_builds")
+    assert ! table_exists?("integrity_notifiers")
+  end
+
+  test "migrating data from initial up to add_commits migration" do
+    pending
+  end
+
+  test "migrating data from migration down to initial migration" do
+    pending
   end
 end
