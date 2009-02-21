1386d5298ee83435cecf0e9278040d8c5ad52ddb
Simon Rozet <simon@rozet.name>
2009-02-21 18:05:46 +0100

    Move webrat helper into its own file

--- test/helpers/acceptance.rb
+++ test/helpers/acceptance.rb
@@ -1,61 +1,6 @@
-require 'webrat/rack'
-require 'sinatra'
-require 'sinatra/test'
-
-set :environment, :test
-disable :run
-disable :reload
-
-Webrat.configuration.instance_variable_set("@mode", :sinatra)
-
-module Webrat
-  class SinatraSession < Session
-    DEFAULT_DOMAIN = "integrity.example.org"
-
-    def initialize(context = nil)
-      super(context)
-      @sinatra_test = Sinatra::TestHarness.new
-    end
-
-    %w(get head post put delete).each do |verb|
-      class_eval <<-METHOD
-        def #{verb}(path, data, headers = {})
-          params = data.inject({}) do |data, (key,value)|
-            data[key] = Rack::Utils.unescape(value)
-            data
-          end
-          headers['HTTP_HOST'] = DEFAULT_DOMAIN
-          @sinatra_test.#{verb}(path, params, headers)
-        end
-      METHOD
-    end
-
-    def response_body
-      @sinatra_test.body
-    end
-
-    def response_code
-      @sinatra_test.status
-    end
-
-    private
-
-    def response
-      @sinatra_test.response
-    end
-
-    def current_host
-      URI.parse(current_url).host || DEFAULT_DOMAIN
-    end
-
-    def response_location_host
-      URI.parse(response_location).host || DEFAULT_DOMAIN
-    end
-  end
-end
-
-require Integrity.root.join("app")
+require File.dirname(__FILE__) / "acceptance/webrat"
 require File.dirname(__FILE__) / "acceptance/git_helper"
+require Integrity.root.join("app")
 
 module AcceptanceHelper
   include FileUtils
