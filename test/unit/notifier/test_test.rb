b756e8cf252430ff5f790a756fea4668c46b85bd
Simon Rozet <simon@rozet.name>
2009-03-14 03:32:05 +0100

    Test for Notifier::Test

---
+++ test/unit/notifier/test_test.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + "/../../helpers"
+require "helpers/acceptance/textfile_notifier"
+
+class NotifierTestTest < Test::Unit::TestCase
+  include Integrity::Notifier::Test
+
+  before(:each) do
+    # Because we unset every notifier in global setup
+    load "helpers/acceptance/textfile_notifier.rb"
+  end
+
+  def notifier
+    "Textfile"
+  end
+
+  test "it provides a formulary to configure options" do
+    assert_form_have_option("file")
+  end
+
+  test "it sends notification" do
+    assert notification.include?(commit.message)
+  end
+end
