284d4c70fda9f781adc14e7106efc32e11546263
Simon Rozet <simon@rozet.name>
2009-03-22 00:17:17 +0100

    Heroku install template cosmetics

--- config/heroku/integrity-config.rb
+++ config/heroku/integrity-config.rb
@@ -5,7 +5,10 @@ require "integrity"
 Integrity.config = {
   :database_uri     => ENV["DATABASE_URL"],
   :export_directory => File.dirname(__FILE__) + "/tmp",
-  :log              => File.dirname(__FILE__) + "/log/integrity.log"
+  :log              => File.dirname(__FILE__) + "/log/integrity.log",
+  # Uncomment to setup a password
+  # :admin_usenrame   => "admin",
+  # :admin_password   => "foobar"
 }
 
 Integrity.new
