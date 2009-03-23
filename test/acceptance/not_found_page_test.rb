6386ab58c99640fed92b14b55cdc4d79de4ce3a0
Simon Rozet <simon@rozet.name>
2009-03-23 23:32:02 +0100

    Test for the 404; some more URLs fixes

---
+++ test/acceptance/not_found_page_test.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + "/helpers"
+
+class NotFoundPageTest < Test::Unit::AcceptanceTestCase
+  story <<-EOS
+    As an visitor,
+    I want to be shown a friendly four oh four
+    So that I DON'T HAVE TO THINK.
+  EOS
+
+  scenario "chilling on some Integrity instance found via The Holy Hub" do
+    project = Project.gen
+
+    visit "/42"
+    assert_equal 404, response_code
+
+    click_link "list of projects"
+    assert_contain(project.name)
+
+    visit "/42"
+
+    click_link "the projects list"
+    assert_contain(project.name)
+
+    visit "/42"
+
+    click_link "back from whence you came"
+    assert_contain("Add a new project")
+  end
+end
