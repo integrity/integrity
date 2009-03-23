5950c37c159f546b480515ed0be0e211b6f9bec2
Simon Rozet <simon@rozet.name>
2009-03-23 23:18:19 +0100

    Test for unauthorized page and one more URL fix
    I am not happy with the tests, though. It remembers me
    we need to get rid of our hackish test helpers, #login and #logout.

---
+++ test/acceptance/unauthorized_page_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + "/helpers"
+
+class UnauthorizedPageTest < Test::Unit::AcceptanceTestCase
+  story <<-EOS
+    As an administrator,
+    I want to be shown a friendly login error page
+    So that I don't feel guilty of loosing my password
+  EOS
+
+  scenario "an administrator (who's amnesiac) tries to login" do
+    project = Project.gen(:public => false)
+
+    visit "/#{project.name}/edit"
+    assert_equal 401, response_code
+
+    # TODO click_link "try again"
+    assert_have_tag("a[@href='/login']", :content => "try again")
+    assert_have_tag("a[@href='/']",      :content => "go back")
+  end
+end
