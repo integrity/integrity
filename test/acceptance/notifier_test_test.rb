5b6007af1576629460dce953fd4ac5a1c5264cc0
Simon Rozet <simon@rozet.name>
2009-04-14 03:39:27 +0200

    Improve Notifier::Test tests

--- test/acceptance/notifier_test_test.rb
+++ test/acceptance/notifier_test_test.rb
@@ -7,19 +7,29 @@ require "integrity/notifier/test"
 class NotifierTestTest < Test::Unit::TestCase
   include Integrity::Notifier::Test
 
+  setup do
+    @notifier = Integrity::Notifier::Textfile
+    @config   = {"file" => "/tmp/integrity.txt"}
+    @build    = Integrity::Build.gen(:successful)
+
+    FileUtils.rm @config["file"] if File.exists?(@config["file"])
+  end
+
   def notifier
     "Textfile"
   end
 
   test "it provides a formulary to configure options" do
-    assert_form_have_option("file")
+    assert_form_have_option("file", @config["file"])
   end
 
   test "it sends notification" do
-    commit = Integrity::Commit.gen(:build => Build.gen(:successful))
+    @notifier.notify_of_build(@build, @config)
 
-    assert notification(commit).include?(commit.identifier)
-    assert notification_failed.include?("failed")
-    assert notification_successful.include?("was successful")
+    notification = File.read(@config["file"])
+
+    assert notification.start_with?("===")
+    assert notification.include?(@build.commit.identifier)
+    assert notification.include?("successful")
   end
 end
