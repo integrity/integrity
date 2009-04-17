3acdcad5ad0a3120ed95977f17ca515f081cd6e5
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-04-17 16:49:25 -0400

    Add :base_uri => nil in config for Heroku
    Also, added a comment for the would-be editor.
    
    Signed-off-by: Simon Rozet <simon@rozet.name>

--- config/heroku/integrity-config.rb
+++ config/heroku/integrity-config.rb
@@ -3,6 +3,7 @@ gem "integrity"
 require "integrity"
 
 Integrity.config = {
+  :base_uri         => nil, # Edit this! Like :base_uri => 'http://awesome-subdomain-13.heroku.com'
   :database_uri     => ENV["DATABASE_URL"],
   :export_directory => File.dirname(__FILE__) + "/tmp",
   :log              => File.dirname(__FILE__) + "/log/integrity.log",
