fc18c02825fb3428537859f0b02fcacc6cb5098c
Douglas F Shearer <dougal.s@gmail.com>
2009-06-05 15:55:01 +0100

    pretty_date helper no longer prints a leading zero for days < 10.
    Signed-off-by: Simon Rozet <simon@rozet.name>

--- test/unit/helpers_test.rb
+++ test/unit/helpers_test.rb
@@ -9,14 +9,14 @@ class BrowsePublicProjectsTest < Test::Unit::TestCase
     @h.pretty_date(Time.now).should == "today"
     @h.pretty_date(Time.new - 86400).should == "yesterday"
 
-    @h.pretty_date(Time.mktime(1995, 12, 01)).should == "on Dec 01st"
+    @h.pretty_date(Time.mktime(1995, 12, 01)).should == "on Dec 1st"
     @h.pretty_date(Time.mktime(1995, 12, 21)).should == "on Dec 21st"
     @h.pretty_date(Time.mktime(1995, 12, 31)).should == "on Dec 31st"
 
     @h.pretty_date(Time.mktime(1995, 12, 22)).should == "on Dec 22nd"
     @h.pretty_date(Time.mktime(1995, 12, 22)).should == "on Dec 22nd"
 
-    @h.pretty_date(Time.mktime(1995, 12, 03)).should == "on Dec 03rd"
+    @h.pretty_date(Time.mktime(1995, 12, 03)).should == "on Dec 3rd"
     @h.pretty_date(Time.mktime(1995, 12, 23)).should == "on Dec 23rd"
 
     @h.pretty_date(Time.mktime(1995, 12, 15)).should == "on Dec 15th"
