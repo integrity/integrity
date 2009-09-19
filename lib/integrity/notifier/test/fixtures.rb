394e90a20c2a29371b3dbc2beacf151fdcec554e
Simon Rozet <simon@rozet.name>
2009-09-19 02:33:27 +0200

    Define fixtures inside of Integrity
    Save some typing and looks cleaner

--- lib/integrity/notifier/test/fixtures.rb
+++ lib/integrity/notifier/test/fixtures.rb
@@ -1,104 +1,106 @@
 require "dm-sweatshop"
 
-include DataMapper::Sweatshop::Unique
-
 class Array
   def pick
     self[rand(self.length)]
   end
 end
 
-Integrity::Project.fixture do
-  { :name       => (name = unique { /\w+/.gen }),
-    :scm        => "git",
-    :uri        => "git://github.com/#{/\w+/.gen}/#{name}.git",
-    :branch     => ["master", "test-refactoring", "lh-34"].pick,
-    :command    => ["rake", "make", "ant -buildfile test.xml"].pick,
-    :public     => [true, false].pick }
-end
+module Integrity
+  extend DataMapper::Sweatshop::Unique
 
-Integrity::Commit.fixture(:with_project) do
-  project = Integrity::Project.first || Integrity::Project.gen
-  Integrity::Commit.generate_attributes.update(:project_id => project.id)
-end
+  Project.fixture do
+    { :name    => (name = unique { /\w+/.gen }),
+      :scm     => "git",
+      :uri     => "git://github.com/#{/\w+/.gen}/#{name}.git",
+      :branch  => %w[master test-refactoring lh-34].pick,
+      :command => %w[rake make ant test.xml].pick,
+      :public  => [true, false].pick }
+  end
 
-Integrity::Project.fixture(:integrity) do
-  { :name       => "Integrity",
-    :scm        => "git",
-    :uri        => "git://github.com/foca/integrity.git",
-    :branch     => "master",
-    :command    => "rake",
-    :public     => true }
-end
+  Project.fixture(:integrity) do
+    { :name    => "Integrity",
+      :scm     => "git",
+      :uri     => "git://github.com/foca/integrity.git",
+      :branch  => "master",
+      :command => "rake",
+      :public  => true }
+  end
 
-Integrity::Project.fixture(:my_test_project) do
-  { :name       => "My Test Project",
-    :scm        => "git",
-    :uri        => File.dirname(__FILE__) + "/../../",
-    :branch     => "master",
-    :command    => "./test",
-    :public     => true }
-end
+  Project.fixture(:my_test_project) do
+    { :name    => "My Test Project",
+      :scm     => "git",
+      :uri     => File.dirname(__FILE__) + "/../../",
+      :branch  => "master",
+      :command => "./test",
+      :public  => true }
+  end
 
-Integrity::Commit.fixture do
-  { :identifier =>   Digest::SHA1.hexdigest(/[:paragraph:]/.gen),
-    :message =>      /[:sentence:]/.gen,
-    :author =>       /\w+ \w+ <\w+@example.org>/.gen,
-    :committed_at => unique {|i| Time.mktime(2008, 12, 15, 18, (59 - i) % 60)} }
-end
+  Commit.fixture do
+    { :identifier => Digest::SHA1.hexdigest(/[:paragraph:]/.gen),
+      :message    => /[:sentence:]/.gen,
+      :author     => /\w+ \w+ <\w+@example.org>/.gen,
+      :committed_at => unique{|i| Time.mktime(2008, 12, 15, 18, (59 - i) % 60)}}
+  end
 
-Integrity::Commit.fixture(:successful) do
-  Integrity::Commit.generate_attributes.update(:build => Integrity::Build.gen(:successful))
-end
+  Commit.fixture(:with_project) do
+    Commit.gen_attrs.update(:project => Project.first || Project.gen)
+  end
 
-Integrity::Commit.fixture(:failed) do
-  Integrity::Commit.generate_attributes.update(:build => Integrity::Build.gen(:failed))
-end
+  Commit.fixture(:successful) do
+    Commit.gen_attrs.update(:build => Build.gen(:successful))
+  end
 
-Integrity::Commit.fixture(:pending) do
-  Integrity::Commit.generate_attributes.update(:build => Integrity::Build.gen(:pending))
-end
+  Commit.fixture(:failed) do
+    Commit.gen_attrs.update(:build => Build.gen(:failed))
+  end
 
-Integrity::Commit.fixture(:building) do
-  Integrity::Commit.generate_attributes.update(:build => Integrity::Build.gen(:building))
-end
+  Commit.fixture(:pending) do
+    Commit.gen_attrs.update(:build => Build.gen(:pending))
+  end
 
-Integrity::Build.fixture do
-  commit = Integrity::Commit.first || Integrity::Commit.gen(:with_project)
+  Commit.fixture(:building) do
+    Commit.gen_attrs.update(:build => Build.gen(:building))
+  end
 
-  { :output       => /[:paragraph:]/.gen,
-    :successful   => true,
-    :started_at   => unique {|i| Time.mktime(2008, 12, 15, 18, i % 60) },
-    :created_at   => unique {|i| Time.mktime(2008, 12, 15, 18, i % 60) },
-    :completed_at => unique {|i| Time.mktime(2008, 12, 15, 18, i % 60) },
-    :commit_id    => commit.id }
-end
+  Build.fixture do
+    commit = Commit.first || Commit.gen(:with_project)
 
-Integrity::Build.fixture(:successful) do
-  Integrity::Build.generate_attributes.update(:successful => true)
-end
+    { :output       => /[:paragraph:]/.gen,
+      :successful   => true,
+      :started_at   => unique {|i| Time.mktime(2008, 12, 15, 18, i % 60) },
+      :created_at   => unique {|i| Time.mktime(2008, 12, 15, 18, i % 60) },
+      :completed_at => unique {|i| Time.mktime(2008, 12, 15, 18, i % 60) },
+      :commit_id    => commit.id }
+  end
 
-Integrity::Build.fixture(:failed) do
-  Integrity::Build.generate_attributes.update(:successful => false)
-end
+  Build.fixture(:successful) do
+    Build.gen_attrs.update(:successful => true)
+  end
 
-Integrity::Build.fixture(:pending) do
-  Integrity::Build.generate_attributes.update(:successful => nil, :started_at => nil, :completed_at => nil)
-end
+  Build.fixture(:failed) do
+    Build.gen_attrs.update(:successful => false)
+  end
 
-Integrity::Build.fixture(:building) do
-  Integrity::Build.generate_attributes.update(:completed_at => nil,
-    :successful => nil, :output => nil)
-end
+  Build.fixture(:pending) do
+    Build.gen_attrs.update(:successful => nil, :started_at => nil,
+      :completed_at => nil)
+  end
 
-Integrity::Notifier.fixture(:irc) do
-  { :project => Integrity::Project.generate,
-    :name => "IRC",
-    :config => { :uri => "irc://irc.freenode.net/integrity" }}
-end
+  Build.fixture(:building) do
+    Build.gen_attrs.update(:completed_at => nil,
+      :successful => nil, :output => nil)
+  end
 
-Integrity::Notifier.fixture(:twitter) do
-  { :project => Integrity::Project.generate,
-    :name => "Twitter",
-    :config => { :email => "foo@example.org", :pass => "secret" }}
+  Notifier.fixture(:irc) do
+    { :project => Project.generate,
+      :name => "IRC",
+      :config => { :uri => "irc://irc.freenode.net/integrity" }}
+  end
+
+  Notifier.fixture(:twitter) do
+    { :project => Project.generate,
+      :name => "Twitter",
+      :config => { :email => "foo@example.org", :pass => "secret" }}
+  end
 end
