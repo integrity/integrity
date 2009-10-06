797aa935719011a17db6fecaa96d1381def79f07
Simon Rozet <simon@rozet.name>
2009-10-06 09:11:17 +0200

    Woops. Avoid rm -rf ~/tmp :-/

--- test/helper.rb
+++ test/helper.rb
@@ -47,7 +47,7 @@ class Test::Unit::TestCase
   before(:all) do
     Integrity.configure { |c|
       c.database  = "sqlite3::memory:"
-      c.directory = File.expand_path(File.dirname(__FILE__) + "/../../../tmp")
+      c.directory = File.expand_path(File.dirname(__FILE__) + "/../tmp")
       c.log  = "/dev/null"
       c.user = "admin"
       c.pass = "test"
