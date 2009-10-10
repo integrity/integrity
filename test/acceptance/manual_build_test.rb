75425f406acc7daa2e6a404e9ee9558386083af5
Simon Rozet <simon@rozet.name>
2009-10-10 16:13:11 +0200

    Alright, so duplicate builds are not an issue
    It's a feature ;-P

--- test/acceptance/manual_build_test.rb
+++ test/acceptance/manual_build_test.rb
@@ -41,7 +41,6 @@ class ManualBuildTest < Test::Unit::AcceptanceTestCase
   end
 
   scenario "Rebuilding three times" do
-    pending("Is this failure really a big deal?") {
     git_repo(:my_test_project).add_successful_commit
     Project.gen(:my_test_project, :uri => git_repo(:my_test_project).uri)
 
@@ -53,8 +52,7 @@ class ManualBuildTest < Test::Unit::AcceptanceTestCase
     click_button "Fetch and build"
 
     assert_have_tag "h1", :content => "success"
-    assert_have_no_tag "#previous_builds"
-    }
+    assert_have_tag "#previous_builds li", :count => 2
   end
 
   scenario "Fixing the build command and then rebuilding HEAD" do
