3e14fd1673d271748bcb475af3beaf4eaccadfdb
Simon Rozet <simon@rozet.name>
2009-10-26 17:58:24 +0100

    Import the campfire notifier
    It's a bit messy and still use integrity/notifier/test but it's not
    a big deal right now

---
+++ lib/integrity/notifier/campfire.rb
@@ -0,0 +1,46 @@
+require "tinder"
+
+module Integrity
+  class Notifier
+    class Campfire < Notifier::Base
+      attr_reader :config
+
+      def self.to_haml
+        File.read(File.dirname(__FILE__) + "/campfire.haml")
+      end
+
+      def deliver!
+        room.speak "#{short_message}. #{build_url}" if announce_build?
+        room.paste full_message if build.failed?
+        room.leave
+      end
+
+    private
+      def room
+        @room ||= begin
+          options = {}
+          options[:ssl] = config["use_ssl"] ? true : false
+          campfire = Tinder::Campfire.new(config["account"], options)
+          campfire.login(config["user"], config["pass"])
+          campfire.find_room_by_name(config["room"])
+        end
+      end
+
+      def full_message
+        <<-EOM
+Commit Message: #{build.commit.message}
+Commit Date: #{build.commit.committed_at}
+Commit Author: #{build.commit.author.name}
+
+#{escape(build.output)}
+EOM
+      end
+
+      def announce_build?
+        build.failed? || config["announce_success"]
+      end
+    end
+
+    register Campfire
+  end
+end
---
+++ test/unit/campfire_notifier_test.rb
@@ -0,0 +1,79 @@
+require "helper"
+require "integrity/notifier/test"
+require "integrity/notifier/campfire"
+require "mocha"
+
+class CampfireNotifierTest < Test::Unit::TestCase
+  include Integrity::Notifier::Test
+
+  setup do
+    Integrity.configure { |c| c.base_uri = "http://example.org" }
+    @config = { "account" => "integrity",
+      "use_ssl" => false,
+      "room"    => "ci",
+      "user"    => "foo",
+      "pass"    => "bar",
+      "announce_success" => true }
+    @notifier = Integrity::Notifier::Campfire
+    @room = stub(:speak => nil, :paste => nil, :leave => nil)
+  end
+
+  def notifier
+    "Campfire"
+  end
+
+  test "it registers itself" do
+    assert_equal @notifier, Integrity::Notifier.available["Campfire"]
+  end
+
+  test "configuration form" do
+    assert provides_option? "account", @config["account"]
+    assert provides_option? "use_ssl", @config["use_ssl"]
+    assert provides_option? "room",    @config["room"]
+    assert provides_option? "user",    @config["user"]
+    assert provides_option? "pass",    @config["pass"]
+    #assert provides_option? "announce_success", @config["announce_success"]
+  end
+
+  test "ssl" do
+    @config["use_ssl"] = true
+
+    Tinder::Campfire.expects(:new).with(@config["account"], { :ssl => true }).
+      returns(stub(:login => true, :find_room_by_name => @room))
+
+    @notifier.notify_of_build(Integrity::Build.gen, @config)
+  end
+
+  test "successful build" do
+    build = Integrity::Build.gen(:successful)
+
+    @notifier.any_instance.stubs(:room).at_least_once.returns(@room)
+    @room.expects(:speak).with {|v| v.include?(build.commit.short_identifier)}
+    @room.expects(:paste).never
+
+    @notifier.notify_of_build(build, @config)
+  end
+
+  test "don't announce successes" do
+    build = Integrity::Build.gen(:successful)
+
+    @config['announce_success'] = false
+    @notifier.any_instance.stubs(:room).at_least_once.returns(@room)
+    @room.expects(:speak).never
+
+    @notifier.notify_of_build(build, @config)
+  end
+
+  test "failed build" do
+    build = Integrity::Build.gen(:failed)
+
+    @notifier.any_instance.stubs(:room).at_least_once.returns(@room)
+    @room.expects(:speak).with {|v| v.include?(build.commit.short_identifier)}
+    @room.expects(:paste).with { |value|
+      value.include?(build.commit.message) &&
+        value.include?(build.output)
+    }
+
+    @notifier.notify_of_build(build, @config)
+  end
+end
