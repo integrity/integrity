81a53f4ca94e28ed7e963b125af514359d7df145
Simon Rozet <simon@rozet.name>
2009-12-21 19:34:58 +0100

    Remove the deprecated config accessors
    Rely on git merge instead of bottering with deprecating stuff

--- lib/integrity/configurator.rb
+++ lib/integrity/configurator.rb
@@ -8,45 +8,16 @@ module Integrity
       DataMapper.setup(:default, uri)
     end
 
-    def database=(uri)
-      warn "c.database = 'db uri' is deprecated; " \
-       "use c.database 'db uri' instead"
-      database(uri)
-    end
-
     def directory(dir)
       Integrity.directory = Pathname(dir)
-    end
-
-    def directory=(dir)
-      warn "c.directory = 'dir' is deprecated; " \
-        "use c.directory 'dir' instead"
-      directory(dir)
     end
 
     def base_url(url)
       Integrity.base_url = Addressable::URI.parse(url)
     end
 
-    def base_url=(url)
-      warn "c.base_url = 'http://example.org' is deprecated; " \
-        "use c.base_url 'http://example.org' instead"
-      base_url(url)
-    end
-
-    def base_uri=(url)
-      warn "c.base_uri is deprecated; use c.base_url instead"
-      base_url(url)
-    end
-
     def log(log)
       Integrity.logger = Logger.new(log)
-    end
-
-    def log=(log)
-      warn "c.log = 'file.log' is deprecated; " \
-        "use c.log 'file.log' instead"
-      log(log)
     end
 
     def builder(name, args=nil)
@@ -76,30 +47,12 @@ module Integrity
       Integrity.app.enable(:build_all)
     end
 
-    def build_all=(v)
-      warn "c.build_all = true is deprecated; " \
-        "use c.build_all! instead"
-      build_all! if v
-    end
-
     def user(v)
       Integrity.app.set(:user, v)
     end
 
-    def user=(u)
-      warn "c.user = 'you' is deprecated; " \
-        "use c.user 'you' instead"
-      user(u)
-    end
-
     def pass(v)
       Integrity.app.set(:pass, v)
-    end
-
-    def pass=(p)
-      warn "c.pass = 'secret' is deprecated; " \
-        "use c.pass 'secret' instead"
-      pass(p)
     end
   end
 end
--- test/unit/configurator_test.rb
+++ test/unit/configurator_test.rb
@@ -11,15 +11,12 @@ class ConfiguratorTest < Test::Unit::TestCase
   end
 
   test "directory" do
-    Integrity.configure { |c| c.directory = "/tmp/builds" }
+    Integrity.configure { |c| c.directory "/tmp/builds" }
     assert_equal "/tmp/builds", Integrity.directory.to_s
   end
 
-  test "base_uri and base_url" do
-    Integrity.configure { |c| c.base_uri = "http://example.org" }
-    assert_equal "http://example.org", Integrity.base_url.to_s
-
-    Integrity.configure { |c| c.base_url = "http://foo.com" }
+  test "base_url" do
+    Integrity.configure { |c| c.base_url "http://foo.com" }
     assert_equal "http://foo.com", Integrity.base_url.to_s
 
     Integrity.base_url = nil
@@ -27,7 +24,7 @@ class ConfiguratorTest < Test::Unit::TestCase
   end
 
   test "log" do
-    Integrity.configure { |c| c.log = "test.log" }
+    Integrity.configure { |c| c.log "test.log" }
     assert_equal "test.log", Integrity.logger.
       instance_variable_get(:@logdev).
       instance_variable_get(:@dev).path
