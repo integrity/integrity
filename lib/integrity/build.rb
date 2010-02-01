d12a39e19797175089ab74282f9db24166fa8a96
Simon Rozet <simon@rozet.name>
2010-02-01 16:46:08 +0100

    Lazy-load Build#output

--- lib/integrity/build.rb
+++ lib/integrity/build.rb
@@ -4,8 +4,7 @@ module Integrity
 
     property :id,           Serial
     property :project_id,   Integer   # TODO :nullable => false
-    property :output,       Text,     :default => "", :lazy => false,
-      :length => 1048576
+    property :output,       Text,     :default => "", :length => 1048576
     property :successful,   Boolean,  :default => false
     property :started_at,   DateTime
     property :completed_at, DateTime
