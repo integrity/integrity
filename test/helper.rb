3394686d9bc055b8fa9a7678cacb14b3b6025a65
Simon Rozet <simon@rozet.name>
2010-02-21 15:21:46 -0800

    bundle exec rake to run the tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,3 @@
-require File.join(File.dirname(__FILE__), "..", ".bundle", "environment")
-
 require "test/unit"
 require "rr"
 require "extlib"
--- test/helper/acceptance.rb
+++ test/helper/acceptance.rb
@@ -1,5 +1,3 @@
-require File.join(File.dirname(__FILE__), "..", "..", ".bundle", "environment")
-
 require "storyteller"
 require "webrat"
 require "rack/test"
