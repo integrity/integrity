integrity/integrity

commit ca1194f6bf8748f450b5beb2eba1c05947f8da93

by-pass hax if repo uri is available in payload

lib/integrity/helpers/push.rb
@@ -30,19 +30,13 @@ module Integrity
       repository = payload.delete("repository")
       branch     = payload.delete("ref").split("/").last
 
-      uri =
+      unless uri = payload.delete("uri")
         if repository["private"]
-          if payload["uri"]
-            payload["uri"]
-          else
-            "git@github.com:#{URI(repository["url"]).path[1..-1]}"
-          end
+          "git@github.com:#{URI(repository["url"]).path[1..-1]}"
         else
           URI(repository["url"]).tap { |u| u.scheme = "git" }.to_s
         end
-
-      # TODO
-      uri = repository["url"] if settings.test?
+      end
 
       commits =
         if settings.build_all?

commit c39f42aff4916c075ca2d41f24ae4a956d5e7bae

fix epic bug introduced in ca1194f

lib/integrity/helpers/push.rb
@@ -31,11 +31,12 @@ module Integrity
       branch     = payload.delete("ref").split("refs/heads/").last
 
       unless uri = payload.delete("uri")
-        if repository["private"]
-          "git@github.com:#{URI(repository["url"]).path[1..-1]}"
-        else
-          URI(repository["url"]).tap { |u| u.scheme = "git" }.to_s
-        end
+        uri =
+          if repository["private"]
+            "git@github.com:#{URI(repository["url"]).path[1..-1]}"
+          else
+            URI(repository["url"]).tap { |u| u.scheme = "git" }.to_s
+          end
       end
 
       commits =

test/acceptance/github_test.rb
@@ -13,7 +13,9 @@ class GitHubTest < Test::Unit::AcceptanceTestCase
 
   def payload(repo)
     { "after"      => repo.head, "ref" => "refs/heads/#{repo.branch}",
-      "repository" => { "url" => repo.uri },
+      # TODO get GitHub to include git URL in its payload :-)
+      # "repository" => { "url" => repo.uri },
+      "uri"        => repo.uri,
       "commits"    => repo.commits }.to_json
   end
 
@@ -67,12 +69,17 @@ class GitHubTest < Test::Unit::AcceptanceTestCase
     repo.add_successful_commit
     Project.gen(:my_test_project, :uri => repo.uri)
 
+    Project.gen(:integrity, :uri => git_repo(:integrity).uri)
+
     github_post payload(repo)
     assert_equal "1", last_response.body
 
     visit "/my-test-project"
 
     assert_have_tag("h1", :content => "Built #{repo.short_head} successfully")
+
+    visit "/integrity"
+    assert_contain "No builds"
   end
 
   scenario "Monitoring the foo/bar branch" do