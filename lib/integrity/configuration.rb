bf6b7cc68c15a1b266f221ba8f461bd977359bda
Simon Rozet <simon@rozet.name>
2010-07-22 00:48:18 +0200

    cleanup Configuration

--- lib/integrity/configuration.rb
+++ lib/integrity/configuration.rb
@@ -1,7 +1,35 @@
 module Integrity
   class Configuration
-    attr_reader :directory, :base_url, :logger, :builder, :github_token,
-      :build_all, :auto_branch, :username, :password
+    attr_reader :directory,
+      :base_url,
+      :builder
+
+    attr_accessor :build_all,
+      :auto_branch,
+      :github_token,
+      :log,
+      :username,
+      :password
+
+    def build_all?
+      !! @build_all
+    end
+
+    def auto_branch?
+      !! @auto_branch
+    end
+
+    def github_enabled?
+      !! @github_token
+    end
+
+    def protected?
+      @username && @password
+    end
+
+    def logger
+      @logger ||= Logger.new(@log)
+    end
 
     def database=(uri)
       DataMapper.setup(:default, uri)
@@ -13,15 +41,6 @@ module Integrity
 
     def base_url=(url)
       @base_url = Addressable::URI.parse(url)
-    end
-
-    # TODO
-    def log=(log)
-      @log = log
-    end
-
-    def logger
-      @logger ||= Logger.new(@log)
     end
 
     def builder=(builder)
@@ -40,42 +59,6 @@ module Integrity
         else
           raise ArgumentError, "Unknown builder #{name}"
         end
-    end
-
-    def github_token=(token)
-      @github_token = token
-    end
-
-    def github_enabled?
-      !! @github_token
-    end
-
-    def build_all=(v)
-      @build_all = v
-    end
-
-    def build_all?
-      !! @build_all
-    end
-
-    def auto_branch=(v)
-      @auto_branch =v
-    end
-
-    def auto_branch?
-      !! @auto_branch
-    end
-
-    def username=(v)
-      @username = v
-    end
-
-    def password=(v)
-      @password = v
-    end
-
-    def protected?
-      @username && @password
     end
   end
 end
