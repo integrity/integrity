ea541a3d53d296c25ba91bf6656cd6e698c63014
Yury Velikanau <yury.spectator@gmail.com>
2011-01-24 15:44:04 +0200

    Integrity and Bundler are real friends now.

--- lib/integrity/command_runner.rb
+++ lib/integrity/command_runner.rb
@@ -21,7 +21,7 @@ module Integrity
       @logger.debug(cmd)
 
       output = ""
-      Bundler.with_clean_env do
+      with_clean_env do
         IO.popen(cmd, "r") { |io| output = io.read }
       end
 
@@ -45,6 +45,30 @@ module Integrity
       else
         "(#{cmd} 2>&1)"
       end
+    end
+
+    BUNDLER_VARS = %w(BUNDLE_GEMFILE RUBYOPT BUNDLE_BIN_PATH)
+
+    # The idea is shamelessly stolen from Bundler.
+    #
+    # In general, Bundler sets several environment variables to operate,
+    # the issue is that parent shell propogate its evironment to
+    # subshells, which breaks builds, because your build is
+    # trying to use Integrity's gemfile.
+    #
+    # So here we unset some of that environment variables to let
+    # build's bundler create its own environment.
+    #
+    # When build is done we rollback to previous environment.
+    #
+    # Bundler team will probably create a way to avoid this, but
+    # most lickely it won't sooner that Bundler 1.1
+    def with_clean_env
+      bundled_env = ENV.to_hash
+      BUNDLER_VARS.each{ |var| ENV.delete(var) }
+      yield
+    ensure
+      ENV.replace(bundled_env.to_hash)
     end
   end
 end
