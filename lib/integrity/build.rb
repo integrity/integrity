a88e1eed25004e73cc1e7e8d31223b5b4169c37a
Rich Meyers <richmeyers@gmx.com>
2011-10-20 00:06:44 -0400

    Handle missing commits in full_message

--- lib/integrity/build.rb
+++ lib/integrity/build.rb
@@ -82,8 +82,12 @@ module Integrity
     end
 
     def full_message
-      # commit.message fallback is here because we don't have migrations (yet?)
-      commit.full_message || commit.message || "message not loaded"
+      if commit
+        # commit.message fallback is here because we don't have migrations (yet?)
+        commit.full_message || commit.message || "message not loaded"
+      else
+        '(commit is missing)'
+      end
     end
 
     def author
