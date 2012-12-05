c5baf5a7975b124cb5935427cb0afc6683d7cb6f
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-05 16:31:13 -0500

    Test artifact retrieval

--- test/acceptance/browse_builds_test.rb
+++ test/acceptance/browse_builds_test.rb
@@ -110,7 +110,7 @@ class BrowseBuildsTest < Test::Unit::AcceptanceTestCase
     assert !File.exist?(build_directory)
     FileUtils.mkdir_p(build_directory)
     File.open(File.join(build_directory, '2f8375806436491fe106e2151edb0ffd'), 'w') do |f|
-      f << '2f8375806436491fe106e2151edb0ffd'
+      f << 'content2f8375806436491fe106e2151edb0ffd'
     end
     
     visit "/integrity"
@@ -120,6 +120,10 @@ class BrowseBuildsTest < Test::Unit::AcceptanceTestCase
     assert_have_tag('h2', :content => 'Artifacts')
     # link to artifact
     assert_have_tag('a', :content => '2f8375806436491fe106e2151edb0ffd')
+    
+    # visit artifact
+    click_link(/2f8375806436491fe106e2151edb0ffd/)
+    assert_contain('content2f8375806436491fe106e2151edb0ffd')
   end
   
   scenario "Browsing a build with a specified but missing wildcard artifact" do
@@ -141,10 +145,10 @@ class BrowseBuildsTest < Test::Unit::AcceptanceTestCase
     assert !File.exist?(build_directory)
     FileUtils.mkdir_p(build_directory)
     File.open(File.join(build_directory, 'artifactb46be9dce08b0a5486342b90e732be49'), 'w') do |f|
-      f << 'b46be9dce08b0a5486342b90e732be49'
+      f << 'contentb46be9dce08b0a5486342b90e732be49'
     end
     File.open(File.join(build_directory, 'artifactc46b6d4e8839b5eb33f739d47268168a'), 'w') do |f|
-      f << 'c46b6d4e8839b5eb33f739d47268168a'
+      f << 'contentc46b6d4e8839b5eb33f739d47268168a'
     end
     
     visit "/integrity"
@@ -155,5 +159,9 @@ class BrowseBuildsTest < Test::Unit::AcceptanceTestCase
     # links to artifacts
     assert_have_tag('a', :content => 'b46be9dce08b0a5486342b90e732be49')
     assert_have_tag('a', :content => 'c46b6d4e8839b5eb33f739d47268168a')
+    
+    # visit artifacts
+    click_link(/c46b6d4e8839b5eb33f739d47268168a/)
+    assert_contain('contentc46b6d4e8839b5eb33f739d47268168a')
   end
 end
