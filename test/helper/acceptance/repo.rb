a77dbca13aa4d21446867a4e586317e759a9e58e
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-29 20:58:21 -0500

    Check that commands execute successfully in the test suite

--- test/helper/acceptance/repo.rb
+++ test/helper/acceptance/repo.rb
@@ -15,30 +15,30 @@ module TestHelper
       FileUtils.mkdir(@path)
 
       Dir.chdir(@path) {
-        `git init`
-        `git config user.name 'John Doe'`
-        `git config user.email 'johndoe@example.org'`
+        run!("git init")
+        run!("git config user.name 'John Doe'")
+        run!("git config user.email 'johndoe@example.org'")
       }
 
       add_commit("First commit") {
-        `echo 'just a test repo' >> README`
-        `git add README`
+        run!("echo 'just a test repo' >> README")
+        run!("git add README")
       }
     end
 
     def add_successful_commit
       add_commit("This commit will work") {
-        `echo '#{script(0)}' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo '#{script(0)}' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
 
     def add_failing_commit
       add_commit("This commit will fail") {
-        `echo '#{script(1)}' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo '#{script(1)}' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
     
@@ -47,9 +47,9 @@ module TestHelper
       subject = '123456789 ' * 200
       message = "#{subject} end-subject\n\nAnd again in body:\n\n#{subject} end-body"
       add_commit(message) {
-        `echo '#{script(0)}' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo '#{script(0)}' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
     
@@ -57,38 +57,38 @@ module TestHelper
       subject = 'Коммит'
       message = "#{subject} end-subject\n\nAnd again in body:\n\n#{subject} end-body"
       add_commit(message) {
-        `echo '#{script(0)}' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo '#{script(0)}' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
 
     def add_commit_with_utf8_command_output
       add_commit("This commit will work") {
-        `echo '#{utf8_script(0)}' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo '#{utf8_script(0)}' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
 
     def add_commit_with_invalid_utf8_command_output
       add_commit("This commit will work") {
-        `echo '#{invalid_utf8_script(0)}' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo '#{invalid_utf8_script(0)}' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
 
     def add_commit_echoing_integrity_branch
       add_commit("This commit echoes INTEGRITY_BRANCH") {
-        `echo 'echo branch=$INTEGRITY_BRANCH' > test`
-        `chmod +x test`
-        `git add test`
+        run!("echo 'echo branch=$INTEGRITY_BRANCH' > test")
+        run!("chmod +x test")
+        run!("git add test")
       }
     end
 
     def head
-      Dir.chdir(@path) { `git log --pretty=format:%H | head -1`.chomp }
+      Dir.chdir(@path) { run!("git log --pretty=format:%H | head -1").chomp }
     end
 
     def short_head
@@ -97,14 +97,14 @@ module TestHelper
 
     def commits
       Dir.chdir(@path) {
-        `git log --pretty=format:%H`.each_line.collect{|l| l.split("\n").first}.
+        run!("git log --pretty=format:%H").each_line.collect{|l| l.split("\n").first}.
         inject([]) { |acc, sha1|
           # Note: psych will return unquoted timestamp as a Time object,
           # syck will return it as a string
           # Note: use single quotes because of how we invoke git below
           fmt  = "---%nmessage: >-%n  %s%ntimestamp: '%ci'%n" \
             "id: %H%nauthor: %n name: %an%n email: %ae%n"
-          acc << YAML.load(`git show -s --pretty=format:"#{fmt}" #{sha1}`)
+          acc << YAML.load(run!(%(git show -s --pretty=format:"#{fmt}" #{sha1})))
         }.reverse
       }
     end
@@ -112,14 +112,14 @@ module TestHelper
     def add_commit(message)
       Dir.chdir(@path) {
         yield
-        `git commit -m "#{@branch}: #{message}" \
-           --author="John Doe <jdoe@gmail.com>"`
+        run!(%(git commit -m "#{@branch}: #{message}" \
+           --author="John Doe <jdoe@gmail.com>"))
       }
     end
 
     def checkout(branch)
       @branch = branch
-      Dir.chdir(@path) { `git checkout -b #{branch} > /dev/null 2>&1` }
+      Dir.chdir(@path) { run!("git checkout -b #{branch} > /dev/null 2>&1") }
     end
 
     def script(status)
@@ -144,6 +144,21 @@ SH
   echo "Bogus \250 UTF-8..."
   exit #{status}
 SH
+    end
+    
+    class CommandFailed < StandardError
+    end
+    
+    def run!(command)
+      output = `#{command}`
+      if $? != 0
+        msg = "Command #{command} failed with code #{$?}"
+        unless output.empty?
+          msg += "\n#{output}"
+        end
+        raise CommandFailed.new(msg)
+      end
+      output
     end
   end
 end
