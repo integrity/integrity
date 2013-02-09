46d269a02d115f27941cbe532d853febce634657
Oleg Pudeyev <oleg@bsdpower.com>
2013-02-09 04:39:49 -0500

    Try to find a usable intermediate encoding

--- lib/integrity.rb
+++ lib/integrity.rb
@@ -55,6 +55,9 @@ DataMapper.finalize
 Addressable::URI.class_eval { def gsub(*a); to_s.gsub(*a); end }
 
 module Integrity
+  class CannotFindEncoding < StandardError
+  end
+  
   autoload :ThreadedBuilder, "integrity/builders/threaded_builder"
   autoload :DelayedBuilder,  "integrity/builders/delayed_builder"
   autoload :ResqueBuilder,   "integrity/builders/resque_builder"
@@ -113,7 +116,7 @@ module Integrity
     # http://stackoverflow.com/questions/9126782/how-to-change-deprecated-iconv-to-stringencode-for-invalid-utf8-correction
     if text.respond_to?(:encoding)
       # ruby 1.9
-      text = text.force_encoding('utf-8').encode('utf-16', :invalid => :replace, :replace => '?').encode('UTF-8')
+      text = text.force_encoding('utf-8').encode(intermediate_encoding, :invalid => :replace, :replace => '?').encode('utf-8')
     else
       # ruby 1.8
       # As no encoding checks are done, any string will be accepted.
@@ -138,5 +141,20 @@ module Integrity
       @iconv_loaded = true
     end
     @iconv
+  end
+  
+  # Apparently utf-16 is not available everywhere, in particular not on travis.
+  # Try to find a usable encoding.
+  def self.intermediate_encoding
+    map = {}
+    Encoding.list.each do |encoding|
+      map[encoding.name.downcase] = true
+    end
+    %w(utf-16 utf-16be utf-16le utf-7 utf-32 utf-32le utf-32be).each do |candidate|
+      if map[candidate]
+        return candidate
+      end
+    end
+    raise CannotFindEncoding, 'Cannot find an intermediate encoding for conversion to UTF-8'
   end
 end
