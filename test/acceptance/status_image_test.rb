30721e0bdb47762854fc6d12e531e523f22434d2
Oleg Pudeyev <oleg@bsdpower.com>
2013-03-21 00:28:21 -0400

    Status image acceptance test

---
+++ test/acceptance/status_image_test.rb
@@ -0,0 +1,34 @@
+require "helper/acceptance"
+
+class StatusImageTest < Test::Unit::AcceptanceTestCase
+  story <<-EOS
+    As a user,
+    I want to retrieve my project and build status as an image,
+    In order to include it in web pages
+  EOS
+
+  scenario "Get public project status image" do
+    successful_project = Project.gen :successful
+
+    get "/#{successful_project.permalink}.png"
+
+    assert_equal 200, response_code
+    assert_equal "image/png", response_content_type
+  end
+
+  scenario "Get private project status image" do
+    private_project = Project.gen :successful, :public => false
+
+    get "/#{private_project.permalink}.png"
+
+    assert_equal 401, response_code
+    assert_equal "text/html;charset=utf-8", response_content_type
+
+    login_as "admin", "test"
+
+    get "/#{private_project.permalink}.png"
+
+    assert_equal 200, response_code
+    assert_equal "image/png", response_content_type
+  end
+end
